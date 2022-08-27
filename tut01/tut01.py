fffff97d13a5cc840a3b486b5fd9d949b2017eca
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-08-27 12:23:41 -0700

    tut01 done

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,13 @@
-def factorial(int x):
-    
+def factorial(x):
+ fa =1
+ if x==0:
+   print("Factorial = 1")
+ elif x<0:
+   print("Invalid Number")
+ else:
+   for i in range (1,x+1):
+      fa=fa*i     
+ print("factorial = ",fa,)  
 
 x=int(input("Enter the number whose factorial is to be found"))
 factorial(x)
