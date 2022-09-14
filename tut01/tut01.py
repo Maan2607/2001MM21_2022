119ae176a998d3a80e60dd129196f21d1c665666
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-09-14 10:27:25 -0700

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -156,4 +156,31 @@ def octact_identification(mod):
    oct6=0
    oct7=0
    oct8=0
+
+
+ with open('octant_output.csv','w',newline="") as file: # opening the file to write the output
+  writer=csv.writer(file)
+  writer.writerow(["Time",'U','V','W','Uavg','Vavg','Wavg',"U'=U-Uavg","V'=V-Vavg","W'=W-Wavg","Octant","","OctantID","+1","-1","+2","-2","+3","-3","+4","-4"])
+  j=0
+  for x in range(n-1):
+   if(x==0):   
+    writer.writerow([time1[x],u1[x],v1[x],w1[x],u_mean,v_mean,w_mean,u2[x],v2[x],w2[x],oct[x],"","Overall count",c1,c2,c3,c4,c5,c6,c7,c8])
+   elif(x==1):
+    s="mod "+str(mod)		
+    writer.writerow([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"User input",s,"","","","","","","",""])
+   elif(x>=2 and x<2+m):
+    if(x==1+m):# it will work ont=ly if x=1+m
+     z=j*mod 	
+     y=(j+1)*mod
+     s=str(z)+"-"+str(n)		 
+     writer.writerow([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"",s,o1[x-2],o2[x-2],o3[x-2],o4[x-2],o5[x-2],o6[x-2],o7[x-2],o8[x-2]])	 
+     j=j+1
+    else:
+     z=j*mod	
+     y=(j+1)*mod-1
+     s=str(z)+"-"+str(y)		 
+     writer.writerow([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"",s,o1[x-2],o2[x-2],o3[x-2],o4[x-2],o5[x-2],o6[x-2],o7[x-2],o8[x-2]])	 
+     j=j+1
+   else:
+    writer.writerow([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"","","","","","","","","",""])			 
  
