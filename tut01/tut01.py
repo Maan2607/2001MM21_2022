87583396e7f5992f3287a93bae19d15d66b25606
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-09-14 10:17:55 -0700

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,14 +1,29 @@
-def octact_identification(mod=5000):
-###Code
+def octact_identification(mod):
+ import csv
 
+ import os
 
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-mod=5000
-octact_identification(mod)
+ import numpy as np
+ os.system('cls')
+ time1 = [] # make a new list
+ v1 = []    # make a new list
+ u1 = []    # make a new list
+ w1 = []    # make a new list
+ v2 = []    # make a list for difference 
+ u2 = []    # make a list for difference
+ w2 = []    # make a list for difference
+ oct = []   # make a list for octant storing
+ with open('octant_input.csv', 'r') as file:
+  reader = csv.reader(file)
+  j = 0
+  for row in reader:
+   if (j == 0):
+    j = j+1 #continue only for j==0
+   else:
+    time1.append(row[0])
+    u1.append(row[1])
+    v1.append(row[2])
+    w1.append(row[3])
+  v_mean=np.mean(v1, dtype=np.float64) # it will give mean of v1
+  u_mean=np.mean(u1, dtype=np.float64) # it will give mean of u1
+  w_mean=np.mean(w1, dtype=np.float64) # it will give mean of w1
