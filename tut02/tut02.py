113a43bfcc55d249c938438539f96ca3a8805db9
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-09-27 12:49:51 -0700

    starting code

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,12 +1,32 @@
 def octant_transition_count(mod=5000):
+ from openpyxl import load_workbook
+ import os
+ import numpy as np
+ os.system('cls')
 
- from platform import python_version
- ver = python_version()
+ time1 = [] # make a new list
+ v1 = []    # make a new list
+ u1 = []    # make a new list
+ w1 = []    # make a new list
+ v2 = []    # make a list for difference 
+ u2 = []    # make a list for difference
+ w2 = []    # make a list for difference
+ oct = []   # make a list for octant storing
 
- if ver == "3.8.10":
-  print("Correct Version Installed")
- else:
-  print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+ wb=load_workbook("input_octant_transition_identify.xlsx")
+ sheet=wb["sheet1"] 
+ n=sheet.max_row
+ for r in range(2,n+1):
+  time1.append(sheet.cell(row=r,column=1).value)
+  u1.append(sheet.cell(row=r,column=2).value)
+  v1.append(sheet.cell(row=r,column=3).value)
+  w1.append(sheet.cell(row=r,column=4).value)
+
+ v_mean=np.mean(v1, dtype=np.float64) # calculate the mean of v1
+ u_mean=np.mean(u1, dtype=np.float64) # mean of u1
+ w_mean=np.mean(w1, dtype=np.float64) # mean of w1
+ 
+
 
 mod=5000
 octant_transition_count(mod)
