8d0e8558c871116a402e2cb764ab77f87017ef97
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-09-27 13:39:58 -0700

    Update tut02.py

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -14,7 +14,7 @@ def octant_transition_count(mod=5000):
  oct = []   # make a list for octant storing
 
  wb=load_workbook("input_octant_transition_identify.xlsx")
- sheet=wb["sheet1"] 
+ sheet=wb["Sheet1"] 
  n=sheet.max_row
  for r in range(2,n+1):
   time1.append(sheet.cell(row=r,column=1).value)
@@ -25,6 +25,59 @@ def octant_transition_count(mod=5000):
  v_mean=np.mean(v1, dtype=np.float64) # calculate the mean of v1
  u_mean=np.mean(u1, dtype=np.float64) # mean of u1
  w_mean=np.mean(w1, dtype=np.float64) # mean of w1
+ 
+ for r in range(2,n+1):
+  u2.append(float(sheet.cell(row=r,column=2).value)-u_mean) # pushing the differences 
+  v2.append(float(sheet.cell(row=r,column=3).value)-v_mean) # pushing the differences
+  w2.append(float(sheet.cell(row=r,column=4).value)-w_mean)	# pushing the differences  
+
+ for i in n-1 :# using this for loop we will get octant
+  if ((u2[i]>=0) and (v2[i]>=0) and (w2[i]>=0 )):
+   oct.append(1)
+  elif((u2[i]>=0) and (v2[i]>=0) and (w2[i]<0 )):
+   oct.append(-1)
+  elif((u2[i]<0) and (v2[i]>=0) and (w2[i]>=0 )):
+   oct.append(2)
+  elif((u2[i]<0) and (v2[i]>=0) and (w2[i]<0 )):
+   oct.append(-2)
+  elif((u2[i]>=0) and (v2[i]<0) and (w2[i]>=0 )):
+   oct.append(4)
+  elif((u2[i]>=0) and (v2[i]<0) and (w2[i]<0 )):
+   oct.append(-4)    
+  elif((u2[i]<0) and (v2[i]<0) and (w2[i]>=0 )):
+   oct.append(3)
+  else:
+   oct.append(-3) 
+     
+  c1=0 # overall count of octant 1
+  c2=0 # overall count of octant -1
+  c3=0 # overall count of octant 2
+  c4=0 # overall count of octant -2
+  c5=0 # overall count of octant 3
+  c6=0 # overall count of octant -3
+  c7=0 # overall count of octant 4
+  c8=0 # overall count of octant -4	
+  
+  for i in n-1:# using this for loop we will get octant
+   if (i<n-1):
+    if (oct[i]==1):
+     c1=c1+1
+    elif(oct[i]==-1):
+     c2=c2+1
+    elif(oct[i]==2):
+     c3=c3+1
+    elif(oct[i]==-2):
+     c4=c4+1
+    elif(oct[i]==3):
+     c5=c5+1
+    elif(oct[i]==-3):
+     c6=c6+1    
+    elif(oct[i]==4):
+     c7=c7+1
+    else:
+     c8=c8+1
+
+ 
  
 
 
