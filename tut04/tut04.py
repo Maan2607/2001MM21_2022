2a7b198804d0f2cd5d7cd7ac6ef218a4359ba8fc
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-10-10 04:15:10 -0700

    starting of tut04

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -2,12 +2,12 @@
 def octant_longest_subsequence_count_with_range():
 ###Code
 
-from platform import python_version
-ver = python_version()
+ from platform import python_version
+ ver = python_version()
 
-if ver == "3.8.10":
+ if ver == "3.8.10":
     print("Correct Version Installed")
-else:
+ else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
 
