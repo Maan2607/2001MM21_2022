68ca64d8f1fccba96398051379407a56f72d5b0e
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-10-15 07:37:26 -0700

    Using the code from tut02

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -5,19 +5,177 @@ start_time = datetime.now()
 
 #Help https://youtu.be/N6PBd4XdnEw
 def octant_range_names(mod=5000):
+ from openpyxl import load_workbook
+ import os
+ import numpy as np
+ os.system('cls')
 
-    
-    octant_name_id_mapping = {"1":"Internal outward interaction", "-1":"External outward interaction", "2":"External Ejection", "-2":"Internal Ejection", "3":"External inward interaction", "-3":"Internal inward interaction", "4":"Internal sweep", "-4":"External sweep"}
+ time1 = [] # make a new list
+ v1 = []    # make a new list
+ u1 = []    # make a new list
+ w1 = []    # make a new list
+ v2 = []    # make a list for difference 
+ u2 = []    # make a list for difference
+ w2 = []    # make a list for difference
+ oct = []   # make a list for octant storing
 
-###Code
+ wb=load_workbook("octant_input.xlsx")
+ sheet=wb["Sheet1"] 
+ n=sheet.max_row
+ for r in range(2,n+1):
+  time1.append(sheet.cell(row=r,column=1).value)
+  u1.append(sheet.cell(row=r,column=2).value)
+  v1.append(sheet.cell(row=r,column=3).value)
+  w1.append(sheet.cell(row=r,column=4).value)
 
-from platform import python_version
-ver = python_version()
+ v_mean=np.mean(v1, dtype=np.float64) # calculate the mean of v1
+ u_mean=np.mean(u1, dtype=np.float64) # mean of u1
+ w_mean=np.mean(w1, dtype=np.float64) # mean of w1
+ 
+ for r in range(2,n+1):
+  u2.append(float(sheet.cell(row=r,column=2).value)-u_mean) # pushing the differences 
+  v2.append(float(sheet.cell(row=r,column=3).value)-v_mean) # pushing the differences
+  w2.append(float(sheet.cell(row=r,column=4).value)-w_mean)	# pushing the differences  
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+ for i in range(0,n-1):# using this for loop we will get octant
+  if ((u2[i]>=0) and (v2[i]>=0) and (w2[i]>=0 )):
+   oct.append(1)
+  elif((u2[i]>=0) and (v2[i]>=0) and (w2[i]<0 )):
+   oct.append(-1)
+  elif((u2[i]<0) and (v2[i]>=0) and (w2[i]>=0 )):
+   oct.append(2)
+  elif((u2[i]<0) and (v2[i]>=0) and (w2[i]<0 )):
+   oct.append(-2)
+  elif((u2[i]>=0) and (v2[i]<0) and (w2[i]>=0 )):
+   oct.append(4)
+  elif((u2[i]>=0) and (v2[i]<0) and (w2[i]<0 )):
+   oct.append(-4)    
+  elif((u2[i]<0) and (v2[i]<0) and (w2[i]>=0 )):
+   oct.append(3)
+  else:
+   oct.append(-3) 
+     
+ c1=0 # overall count of octant 1
+ c2=0 # overall count of octant -1
+ c3=0 # overall count of octant 2
+ c4=0 # overall count of octant -2
+ c5=0 # overall count of octant 3
+ c6=0 # overall count of octant -3
+ c7=0 # overall count of octant 4
+ c8=0 # overall count of octant -4	
+
+ for i in range(0,n-1):# using this for loop we will get octant
+  if (oct[i]==1):
+   c1=c1+1
+  elif(oct[i]==-1):
+   c2=c2+1
+  elif(oct[i]==2):
+   c3=c3+1
+  elif(oct[i]==-2):
+   c4=c4+1
+  elif(oct[i]==3):
+   c5=c5+1
+  elif(oct[i]==-3):
+   c6=c6+1    
+  elif(oct[i]==4):
+   c7=c7+1
+  else:
+   c8=c8+1
+
+ o1=[] #make a list of octant count in mod range 
+ o2=[] #make a list of octant count in mod range
+ o3=[] #make a list of octant count in mod range
+ o4=[] #make a list of octant count in mod range
+ o5=[] #make a list of octant count in mod range
+ o6=[] #make a list of octant count in mod range
+ o7=[] #make a list of octant count in mod range
+ o8=[] #make a list of octant count in mod range
+
+ oct1=0 #making variables for using in function 
+ oct2=0 #making variables for using in function
+ oct3=0 #making variables for using in function
+ oct4=0 #making variables for using in function
+ oct5=0 #making variables for using in function
+ oct6=0 #making variables for using in function
+ oct7=0 #making variables for using in function
+ oct8=0 #making variables for using in function
+
+ m= int((n-2)/mod) +1 # m will give no.of interval
+
+ for i in range(m):
+  s=mod*i
+  for j in range(mod):	
+   if(j+s<n-1):	                                    
+    if (oct[j+s]==1):
+     oct1=oct1+1 
+    elif(oct[j+s]==-1):
+     oct2=oct2+1
+    elif(oct[j+s]==2):
+     oct3=oct3+1
+    elif(oct[j+s]==-2):
+     oct4=oct4+1
+    elif(oct[j+s]==3):
+     oct5=oct5+1
+    elif(oct[j+s]==-3):
+     oct6=oct6+1    
+    elif(oct[j+s]==4):
+     oct7=oct7+1
+    elif(oct[j+s]==-4):
+     oct8=oct8+1 
+  o1.append(oct1)
+  o2.append(oct2) 
+  o3.append(oct3)
+  o4.append(oct4)
+  o5.append(oct5)
+  o6.append(oct6)
+  o7.append(oct7)
+  o8.append(oct8)
+  oct1=0
+  oct2=0
+  oct3=0
+  oct4=0
+  oct5=0
+  oct6=0
+  oct7=0
+  oct8=0
+
+ from openpyxl import Workbook 
+ book=Workbook()
+ sheet= book.active    
+ rows=[] 
+ rows.append(["Time",'U','V','W','Uavg','Vavg','Wavg',"U'=U-Uavg","V'=V-Vavg","W'=W-Wavg","Octant","","OctantID","+1","-1","+2","-2","+3","-3","+4","-4"])
+ j=0
+ a=0
+ b=0
+ for x in range(n-2):
+  if(x==0):
+   rows.append([time1[x],u1[x],v1[x],w1[x],u_mean,v_mean,w_mean,u2[x],v2[x],w2[x],oct[x],"","Overall count",c1,c2,c3,c4,c5,c6,c7,c8])
+  elif(x==1):
+   s="mod "+str(mod)		
+   rows.append([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"User input",s,"","","","","","","",""])
+  elif(x>=2 and x<2+m):
+   if(x==1+m):# it will work ont=ly if x=1+m
+    z=j*mod 	
+    y=(j+1)*mod
+    s=str(z)+"-"+str(n-2)		 
+    rows.append([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"",s,o1[x-2],o2[x-2],o3[x-2],o4[x-2],o5[x-2],o6[x-2],o7[x-2],o8[x-2]])	 
+    j=j+1
+   else:
+    z=j*mod	
+    y=(j+1)*mod-1
+    s=str(z)+"-"+str(y)		 
+    rows.append([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"",s,o1[x-2],o2[x-2],o3[x-2],o4[x-2],o5[x-2],o6[x-2],o7[x-2],o8[x-2]])	 
+    j=j+1
+  else:
+   rows.append([time1[x],u1[x],v1[x],w1[x],"","","",u2[x],v2[x],w2[x],oct[x],"","","","","","","","",""])
+
+ for i in rows:
+  sheet.append(i)
+ book.save("octant_output_ranking_excel.xlsx")
+      
+ octant_name_id_mapping = {"1":"Internal outward interaction", "-1":"External outward interaction", "2":"External Ejection", "-2":"Internal Ejection", "3":"External inward interaction", "-3":"Internal inward interaction", "4":"Internal sweep", "-4":"External sweep"}
+
+
 
 
 mod=5000 
