0a7e2d5e83498c32a4670364050b589e7197306f
Maan2607 <101566688+Maan2607@users.noreply.github.com>
2022-11-14 23:13:59 -0800

    tut06_completed with update

---
+++ tut06/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+for i in range(0,n): 
+#    print("sssssssssssssssss") 
+#    z=[roll_no[i],stu_name[i]]
+#    c=0
+#    for q in range(0,s):
+#     z.append(dic[m[i][q][1]])
+#     if dic[m[i][q][1]]==1:
+#      c=c+1
+#    z.append(s)
+#    z.append(c)
+#    z.append("")
+#    row.append(z)
--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,107 +1,120 @@
 
+
+## THIS CODE WILL TAKE A BIT LONGER TIME APPROX. = 1 MINUTE
 from datetime import datetime
 from unicodedata import name
 start_time = datetime.now()
 
 def attendance_report():
 ###Code
- import csv
- import os
- import numpy as np
+ import csv #importing csv
+ import os  #importing os 
+ import numpy as np # importing numpy
  os.system('cls')
- roll_no=[]
- stu_name=[]
+ roll_no=[] #declaring a list of registered roll NO.
+ stu_name=[] #declaring a list of registered Name
 
  with open('input_registered_students.csv', 'r') as file:
   reader = csv.reader(file)
   n=0
   for row in reader:
    if n!=0:
-    roll_no.append(row[0])
-    stu_name.append(row[1])
+    roll_no.append(row[0])  #making a list of registered roll NO.
+    stu_name.append(row[1])   #making a list of registered Name
    n=n+1 
   n=n-1
- dat =["28/07","01/08","04/08","08/08","11/08","18/08","22/08","25/08","29/08","01/09","05/09","08/09","12/09"]
+ dat =["28/07","01/08","04/08","08/08","11/08","15/08","18/08","22/08","25/08","29/08","01/09","05/09","08/09","12/09","15/09","26/09","29/09"]
+ # dat list will give all the dates on which classes were conducted
+ s=len(dat) 
 
- real_count={}
- fake_count={}
- actual_count={}
 
- for x in range(0,n):
-  real_count[roll_no[x]]=0 
-  fake_count[roll_no[x]]=0
-  actual_count[roll_no[x]]=0
  with open('input_attendance.csv', 'r') as file:
   reader = csv.reader(file) 
-  x=0
-  for row in reader: 
-   if x==0:
-    x = x+1
-    continue
-   else:
-    if (row[0][0:5] in dat) and row[0][11:13]=="14" :
-     if row[1][0:8] in roll_no:  
-      real_count.update({row[1][0:8]: real_count.get(row[1][0:8])+1})
-    elif row[1][0:8] in roll_no :
-     fake_count.update({row[1][0:8]: fake_count.get(row[1][0:8])+1})
+
+  m=[] # This list will give full data of all Students
+  c=[] # This list will give full data of a particular Student
+  d=[] # This list will give full data of a particular Student on a particular date 
   for x in roll_no:
+   c=[] # Initializing c
    for j in dat:
+    d=[] # Initializing d
+    a=0
+    b=0
+    e=0
     with open('input_attendance.csv', 'r') as file:
      reader = csv.reader(file)
      for row in reader:
-      if j==row[0][0:5] and row[1][0:8]==x and row[0][11:13]=="14" : 
-       actual_count.update({row[1][0:8]: actual_count.get(row[1][0:8])+1})
-       break 
- print(actual_count["2001CB02"])
-
+      if j==row[0][0:5] and row[1][0:8]==x and (row[0][11:13]=="14" or row[0][11:16]=="15:00" ) and a==0 : 
+       a=a+1 # counting real Attendance 
+      elif j==row[0][0:5] and row[1][0:8]==x and (row[0][11:13]=="14" or row[0][11:16]=="15:00") and a>0 : 
+       b=b+1 # counting duplicate Attendance 
+      elif j==row[0][0:5] and row[1][0:8]==x :
+       e=e+1 # counting fake Attendance 
+     d.append(a+b+e)  
+     d.append(a)
+     d.append(b)
+     d.append(e)
+     if a==0:
+      d.append(1)
+     else:
+      d.append(0) 
+    c.append(d)
+   m.append(c)
+ 
  if os.path.exists("output"):
   for f in os.listdir("output"):
-    os.remove(os.path.join("output",f))
+    os.remove(os.path.join("output",f)) # removing all the prebuild files in output folder
 
  os.chdir("output")
- 
- for i in range(0,n):
-  with open( roll_no[i] + '.csv','w',newline="") as file: # opening the file to write the output
-   writer=csv.writer(file)
-   a=(actual_count[roll_no[i]]/13)*100
-   writer.writerow(["Roll no.","Name","total_leacture_taken","attendance_count_actual","attendance_count_fake","attendance_count_absent","Percentage (attendance_count_actual/total_lecture_taken) 2 digit decimal "])
-   writer.writerow([roll_no[i],stu_name[i],13,actual_count[roll_no[i]],fake_count[roll_no[i]],13-actual_count[roll_no[i]],"{:.2f}".format(a)])
+ from openpyxl import Workbook
+ for i in range(0,n): 
+  book=Workbook()
+  sheet= book.active    
+  rows=[] # Making of list of rows of a particular student of all dates
+  rows.append(["Date","Roll No.","Name","Total attendance count","Real","Duplicate","Invalid","absent"])
+  rows.append(["",roll_no[i],stu_name[i],"","","","",""])
+  for q in range(0,s):
+   rows.append([dat[q],"","",m[i][q][0],m[i][q][1],m[i][q][2],m[i][q][3],m[i][q][4]]) # Appending all types of Attendance in row
+  for w in rows:
+   sheet.append(w)
+  book.save( roll_no[i] + ".xlsx") # Saving a file with the name of roll no. for each rgistered student
+   
+  dic={0:"A",1:"P"} # Dictionary for present and absent
+  book=Workbook()
+  sheet= book.active    
+  rows=[]
+  z=["Roll No.","Name"] # Initializing 1st row
+  for i in dat: # Initializing 1st row
+   z.append(i)
+  z.append("Total Lecture taken")
+  z.append("Total Real")
+  z.append("% Attendance")
+  rows.append(z) 
 
- with open( "attendence_report_consolidated" '.csv','w',newline="") as file: # opening the file to write the output
-  writer=csv.writer(file)
-  writer.writerow(["Roll no.","Name","total_leacture_taken","attendance_count_actual","attendance_count_fake","attendance_count_absent","Percentage (attendance_count_actual/total_lecture_taken) 2 digit decimal "])
-  for i in range(0,n): 
-   a=(actual_count[roll_no[i]]/13)*100
-   writer.writerow([roll_no[i],stu_name[i],13,actual_count[roll_no[i]],fake_count[roll_no[i]],13-actual_count[roll_no[i]],"{:.2f}".format(a)])
+  z=["(Sorted by roll no.)","","Atleast one real P"] # Initializing 2nd row
+  for i in range(0,s-1): # using this loop we are making 2nd row
+   z.append("")
+  z.append("(=Total Mon+Thur dynamic count")
+  z.append("")
+  z.append("Real/Actual Lecture taken")
+  rows.append(z) 
 
+  for i in range(0,n):  # using this loop i am making full data of all the students
+   z=[roll_no[i],stu_name[i]]
+   c=0
+   for q in range(0,s):
+    z.append(dic[m[i][q][1]])
+    if dic[m[i][q][1]]=="P":
+     c=c+1
+   z.append(s)
+   z.append(c)
+   l=(c/s)*100
+   z.append("{:.2f}".format(l))
+   rows.append(z)
 
- dup=[]
- s=["Timestamp","Roll no.","Name","total count of attendence on that day"]
- dup.append(s)
- d=0
- st=""
- q=0
- for x in roll_no:  
-  for j in dat:
-   d=0
-   s=[]
-   with open('C:/Users/DELL/Documents/GitHub/2001MM21_2022/tut06/input_attendance.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-     if j==row[0][0:5] and row[1][0:8]==x and row[0][11:13]=="14" : 
-      d=d+1
-      if d==1:
-       st=row[0]
-    if d>1:   
-     s=[st,roll_no[q],stu_name[q],d]
-     dup.append(s)
-  q=q+1  
-
- with open( "attendence_report_duplicate" +'.csv','w',newline="") as file: # opening the file to write the output
-  writer=csv.writer(file)
-  g=len(dup)      
-  for i in range(0,g):
-   writer.writerow([dup[i][0],dup[i][1],dup[i][2],dup[i][3]])  
+  for w in rows:
+   sheet.append(w)
+  book.save( "Attendance_report_consolidated" + ".xlsx")  # Making of a full report of all the students
 
 attendance_report()
 
